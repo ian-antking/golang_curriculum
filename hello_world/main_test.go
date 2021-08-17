72da8b025d6b1efd8b03e8fc00ded7797ddbc0d9
Ian King <ian.antking@gmail.com>
2021-08-17 11:19:49 +0100

    add helper function for tests

--- hello_world/main_test.go
+++ hello_world/main_test.go
@@ -2,31 +2,33 @@ package main
 
 import "testing"
 
+func assertCorrectMessage(test testing.TB, expected string, actual string) {
+	test.Helper()
+	if expected != actual {
+		test.Errorf("expected %q but got %q", expected, actual)
+	}
+}
+
 func TestHello(test *testing.T) {
+
 	test.Run("given no arguments, returns 'Hello World!'", func(test *testing.T) {
 		const expected = "Hello World!"
 		var actual string = hello();
 
-		if expected != actual {
-			test.Errorf("expected %q but got %q", expected, actual)
-		}
+		assertCorrectMessage(test, expected, actual)
 	})
 
 	test.Run("given a string as an argument, returns 'Hello STRING!'", func(test *testing.T) {
 		const expected = "Hello Ian!"
 		var actual string = hello("Ian")
 
-		if expected != actual {
-			test.Errorf("expected %q but got %q", expected, actual)
-		}
+		assertCorrectMessage(test, expected, actual)
 	})
 
 	test.Run("given multiple strings as arguments, returns 'Hello FIRST_STRING!'", func(test *testing.T) {
 		const expected = "Hello Dave!"
 		var actual string = hello("Dave", "Ian", "James")
 
-		if expected != actual {
-			test.Errorf("expected %q but got %q", expected, actual)
-		}	
+		assertCorrectMessage(test, expected, actual)
 	})
 }
