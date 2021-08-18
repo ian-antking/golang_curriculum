6dd94b856e736fce46f18a108ce398bf56cba762
Ian King <ian.antking@gmail.com>
2021-08-18 09:35:41 +0100

    make struct properties public

--- tickets_to_mars/main.go
+++ tickets_to_mars/main.go
@@ -4,6 +4,14 @@ import "fmt"
 import "github.com/ian-antking/golang_curriculum/tickets_to_mars/tickets"
 
 func main() {
-	fmt.Printf("%+v", tickets.GenerateTicket())
+	var generatedTickets []tickets.Ticket
+	for count := 0; count <= 10; count += 1 {
+		generatedTickets = append(generatedTickets, tickets.GenerateTicket())
+	}
 
+	for index := 0; index < len(generatedTickets); index += 1 {
+		ticket := generatedTickets[index]
+		ticketLine := fmt.Sprintf("%v  %v %v $ %v", ticket.Company, ticket.Days, ticket.Service, ticket.Price)
+		fmt.Println(ticketLine)
+	}
 }
--- tickets_to_mars/tickets/tickets.go
+++ tickets_to_mars/tickets/tickets.go
@@ -6,19 +6,19 @@ import "time"
 var companies = [3]string{"Virgin Galactic", "Space X", "Space Adventures"}
 var services = [2]string{"round-trip", "one-way"}
 
-type ticket struct {
-	company string
-	days int
-	service string
-	price int
+type Ticket struct {
+	Company string
+	Days int
+	Service string
+	Price int
 }
 
-func GenerateTicket() ticket {
-	return ticket{
-		company: companies[rand.Intn(len(companies))],
-		days: rand.Intn(365),
-		service: services[rand.Intn(len(services))],
-		price: rand.Intn(100),
+func GenerateTicket() Ticket {
+	return Ticket{
+		Company: companies[rand.Intn(len(companies))],
+		Days: rand.Intn(365),
+		Service: services[rand.Intn(len(services))],
+		Price: rand.Intn(100),
 	}
 }
 
