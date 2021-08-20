52178c08908c75df672af1c80e1ab8c0d4300c21
Ian King <ian.antking@gmail.com>
2021-08-20 17:04:28 +0100

    add Shift module

---
+++ vigenere_cypher/shift/shift.go
@@ -0,0 +1,18 @@
+package shift
+
+import (
+	"unicode"
+	"errors"
+)
+
+func Shift(character rune, spaces uint) (newCharacter rune, err error){
+	character = unicode.ToLower(character)
+	if character > 122 || character < 92 {
+		err = errors.New("character to shift must be a-z")
+	}
+	newCharacter = character + rune(spaces)
+	if newCharacter > 122 {
+		newCharacter -= 26
+	}
+	return
+}
---
+++ vigenere_cypher/shift/shift_test.go
@@ -0,0 +1,42 @@
+package shift
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestShift(t *testing.T) {
+	t.Run("given a character a-z and an integer of 1, returns shifted character", func(t *testing.T) {
+		const expected = 'b'
+		actual, _ := Shift('a', 1)
+
+		assert.Equal(t, expected, actual)
+	})
+
+	t.Run("can shift a character multiple times", func(t *testing.T) {
+		const expected = 'e'
+		actual, _ := Shift('a', 4)
+
+		assert.Equal(t, expected, actual)
+	})
+
+	t.Run("will reset to 'a' if shifted past 'z'", func(t *testing.T) {
+		const expected = 'a'
+		actual, _ := Shift('z', 1)
+
+		assert.Equal(t, expected, actual)
+	})
+
+	t.Run("will convert uppercase letters to lowercase", func(t *testing.T) {
+		const expected = 'a'
+		actual, _ := Shift('A', 0)
+
+		assert.Equal(t, expected, actual)
+	})
+
+	t.Run("will throw if given a non alpha character", func(t *testing.T) {
+		_, err := Shift('!', 3)
+
+		assert.Error(t, err)
+	})
+}
