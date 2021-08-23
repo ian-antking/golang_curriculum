535bcfdfca4753163e506ea15ea3bfc3da5f0ea0
Ian King <ian.antking@gmail.com>
2021-08-23 14:33:14 +0100

    GenerateTicket selects company and service from slices

--- tickets_to_mars/tickets/tickets_test.go
+++ tickets_to_mars/tickets/tickets_test.go
@@ -6,12 +6,27 @@ import (
 )
 
 func TestTickets(t *testing.T) {
-	t.Run("returns a ticket struct", func(t *testing.T) {
-		companies := []string{ "company1" }
-		services  := []string{ "service1" }
+	t.Run("returns a Ticket struct", func(t *testing.T) {
+		companies := []string{ "company0" }
+		services  := []string{ "service0" }
 		stubRandInt := func(number int) int {
 			return number
 		}
+		ticket := GenerateTicket(companies, services, 1, 1, stubRandInt)
+
+		assert.Equal(t, "company0", ticket.Company)
+		assert.Equal(t, 1, ticket.Days)
+		assert.Equal(t, "service0", ticket.Service)
+		assert.Equal(t, 1, ticket.Price)
+	})
+
+	t.Run("can will select company and service from slices", func(t *testing.T) {
+		companies := []string{ "company0", "company1", "company2" }
+		services  := []string{ "service0", "service1", "service2" }
+		stubRandInt := func(number int) int {
+			return 1
+		}
+
 		ticket := GenerateTicket(companies, services, 1, 1, stubRandInt)
 
 		assert.Equal(t, "company1", ticket.Company)
