97ebefc45182982beeaf2eb20d9407260e5f0cd3
Ian King <ian.antking@gmail.com>
2021-08-23 14:48:02 +0100

    wire Format function into main

--- tickets_to_mars/main.go
+++ tickets_to_mars/main.go
@@ -1,17 +1,26 @@
 package main
 
-import "fmt"
-import "github.com/ian-antking/golang_curriculum/tickets_to_mars/tickets"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+	"github.com/ian-antking/golang_curriculum/tickets_to_mars/tickets"
+)
+
+var companies []string = []string{"Virgin Galactic", "Space X", "Space Adventures"}
+var services []string =  []string{"round-trip", "one-way"}
 
 func main() {
 	var generatedTickets []tickets.Ticket
 	for count := 0; count <= 10; count += 1 {
-		generatedTickets = append(generatedTickets, tickets.GenerateTicket())
+		generatedTickets = append(generatedTickets, tickets.Generate(companies, services, 365, 100, rand.Intn))
 	}
 
-	for index := 0; index < len(generatedTickets); index += 1 {
-		ticket := generatedTickets[index]
-		ticketLine := fmt.Sprintf("%v  %v %v $ %v", ticket.Company, ticket.Days, ticket.Service, ticket.Price)
-		fmt.Println(ticketLine)
+	for _, ticket := range generatedTickets {
+		fmt.Println(tickets.Format(ticket))
 	}
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
 }
