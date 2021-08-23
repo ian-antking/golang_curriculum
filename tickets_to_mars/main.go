fca46741d3a8da7db972ef1de5d78bb1184d3323
Ian King <ian.antking@gmail.com>
2021-08-23 15:06:13 +0100

    move companies and services to config.json

--- tickets_to_mars/main.go
+++ tickets_to_mars/main.go
@@ -1,26 +1,36 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"math/rand"
 	"time"
+	"os"
 	"github.com/ian-antking/golang_curriculum/tickets_to_mars/tickets"
 )
 
-var companies []string = []string{"Virgin Galactic", "Space X", "Space Adventures"}
-var services []string =  []string{"round-trip", "one-way"}
+type configMap struct {
+	Companies []string
+	Services []string
+	MaxDuration int
+	MaxPrice int
+}
+
+var config configMap
+
+func Init() {
+	rand.Seed(time.Now().UnixNano())
+}
 
 func main() {
+	configJson, _ := os.ReadFile("config.json")
+	json.Unmarshal(configJson, &config)
 	var generatedTickets []tickets.Ticket
 	for count := 0; count <= 10; count += 1 {
-		generatedTickets = append(generatedTickets, tickets.Generate(companies, services, 365, 100, rand.Intn))
+		generatedTickets = append(generatedTickets, tickets.Generate(config.Companies, config.Services, 365, 100, rand.Intn))
 	}
 
 	for _, ticket := range generatedTickets {
 		fmt.Println(tickets.Format(ticket))
 	}
-}
-
-func init() {
-	rand.Seed(time.Now().UnixNano())
 }
